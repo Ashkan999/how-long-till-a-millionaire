8bec5a29b48fdb6d094108b68f64bf78a286be63
Ashkan <ashkan1998@hotmail.com>
2021-02-09 23:24:33 +0100

    Add responsive mobile menu

--- src/components/home_page/Header.js
+++ src/components/home_page/Header.js
@@ -1,35 +1,58 @@
-import React from "react";
-import { CgMenu } from "react-icons/cg";
+import { useState } from "react";
+import { CgMenu, CgClose } from "react-icons/cg";
 import { FaMoneyBillWave } from "react-icons/fa";
 import { Link, NavLink } from "react-router-dom";
 
 function Header() {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const openMobileMenu = () => setMobileMenuOpen(true);
+  const closeMobileMenu = () => setMobileMenuOpen(false);
+
   return (
     <header>
       <Link to="/" className="logo">
         <FaMoneyBillWave className="icon" />
         <p>How Long Till I'm a Millionaire</p>
       </Link>
-      <nav>
+      <nav
+        className={isMobileMenuOpen ? "nav-container-mobile" : "nav-container"}
+      >
+        {isMobileMenuOpen && (
+          <CgClose className="close-menu-icon" onClick={closeMobileMenu} />
+        )}
         <ul className="nav-links">
           <li>
-            <NavLink exact to="/" activeClassName="navlink-active">
+            <NavLink
+              exact
+              to="/"
+              activeClassName="navlink-active"
+              onClick={closeMobileMenu}
+            >
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink to="/about" activeClassName="navlink-active">
+            <NavLink
+              to="/about"
+              activeClassName="navlink-active"
+              onClick={closeMobileMenu}
+            >
               About
             </NavLink>
           </li>
           <li>
-            <NavLink to="/contact" activeClassName="navlink-active">
+            <NavLink
+              to="/contact"
+              activeClassName="navlink-active"
+              onClick={closeMobileMenu}
+            >
               Contact
             </NavLink>
           </li>
         </ul>
       </nav>
-      <CgMenu className="mobile-menu" />
+      <CgMenu className="mobile-menu-icon" onClick={openMobileMenu} />
     </header>
   );
 }
