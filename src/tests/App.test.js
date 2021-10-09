304cf49559dea9f5b7538096049ecebde75c7807
Ashkan <ashkan1998@hotmail.com>
2021-10-09 01:31:47 -0500

    Remove app test

--- src/tests/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import React from "react";
-import App from "../App";
-
-test("renders welcome text", () => {
-  render(<App />);
-  const welcomeText = screen.getByText("How Long Till I'm a Millionaire");
-  expect(welcomeText).toBeInTheDocument();
-});
